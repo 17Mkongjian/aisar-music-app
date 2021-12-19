f1b250275c6e3c2a39e62c79a99ab03b52355424
usrtname//(17Mkongjian) <username@email.com>
2021-12-19 16:30:53 +0800

    import:quasar

--- Dockerfile
+++ Dockerfile
@@ -12,4 +12,4 @@ FROM nginx:stable-alpine as production-stage
 COPY --from=build-stage /app/dist /usr/share/nginx/html
 
 EXPOSE 8080
-CMD [ "nginx"," http-server", "dist", "-g"]
+CMD [ "nginx","http-server", "dist", "-g"]
--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { Quasar } from 'quasar'
 
-createApp(App).mount('#app')
+import '@quasar/extras/material-icons/material-icons.css'
+
+import 'quasar/src/css/index.sass'
+const musicApp = createApp(App)
+
+musicApp.use(Quasar,{
+  plugins: {},
+})
+
+musicApp.mount('#app')
